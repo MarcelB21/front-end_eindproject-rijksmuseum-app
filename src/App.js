5060805c0b9888483b12e9290d855ac7eeaf39e9
MarcelB21 <marcelbrzezinski@msn.com>
2021-12-30 17:11:07 +0100

    add-switch/route-and-redirect-to-home-in-app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
