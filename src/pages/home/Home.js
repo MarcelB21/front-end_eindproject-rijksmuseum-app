de3787e0a190af872fa1c45fafea0114004402bc
MarcelB21 <marcelbrzezinski@msn.com>
2021-12-31 08:36:21 +0100

    insert-url-and-apikey-and-useeffect-in-home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,24 @@
 import './Home.css';
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 
 const Home = () => {
 
-    async function fetchData() {
-        try {
-            const result = await axios.get();
-        } catch (e) {
-            console.error(e);
+    const apiKey = 'IofFTSpv';
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&involvedMaker=Rembrandt+van+Rijn`);
+                console.log(result);
+            } catch (e) {
+                console.error(e);
+            }
         }
-    }
     fetchData()
+    }, [])
+
+
 
 return (
     <div>
