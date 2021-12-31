4c1bcc1a0277a4ab0c110e79a9fc473f8bf1304d
MarcelB21 <marcelbrzezinski@msn.com>
2021-12-31 09:48:38 +0100

    adding-art-info-in-home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -26,7 +26,22 @@ const Home = () => {
 
     return (
         <div>
-
+            {art.map((art)=>{
+                return (
+                    <>
+                        <div className="art-container">
+                            <ul className="art-body">
+                                <h2>Title:</h2>
+                                <a>{art.title}</a>
+                                <h3>image:</h3>
+                                <img src={art.webImage.url} alt="art-image" width="250px"/>
+                                <h4>ID: </h4>
+                                <p>{art.id}</p>
+                            </ul>
+                        </div>
+                    </>
+                )
+            })}
         </div>
     );
 };
