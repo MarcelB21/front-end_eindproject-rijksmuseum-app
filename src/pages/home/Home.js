92942cc49f18276457a0f081436ede60fe96578f
MarcelB21 <marcelbrzezinski@msn.com>
2021-12-31 09:08:27 +0100

    add-usestate-in-home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,30 +1,34 @@
 import './Home.css';
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 const Home = () => {
 
+    const [art, setArt] = useState('');
+    
     const apiKey = 'IofFTSpv';
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const result = await axios.get(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&involvedMaker=Rembrandt+van+Rijn`);
-                console.log(result);
+                console.log(result.data.artObjects);
+                setArt(result.data.artObjects);
             } catch (e) {
                 console.error(e);
             }
         }
+
     fetchData()
     }, [])
 
 
 
-return (
-    <div>
+    return (
+        <div>
 
-    </div>
-);
+        </div>
+    );
 };
 
 export default Home;
