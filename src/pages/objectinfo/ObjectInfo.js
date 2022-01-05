3eb3c993810c6e791a24b2efb313ca576a1bfcaa
MarcelB21 <marcelbrzezinski@msn.com>
2022-01-05 15:51:38 +0100

    troubleschooting3-and-add-art-info-objectinfo.js

--- src/pages/objectinfo/ObjectInfo.js
+++ src/pages/objectinfo/ObjectInfo.js
@@ -1,29 +1,44 @@
 import './ObjectInfo.css';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {useParams} from "react-router-dom";
 
 const ObjectInfo = () => {
 
+    const [artInfo, setArtInfo] = useState();
+    const [art, setArt] = useState([]);
     const apiKey = 'IofFTSpv';
-    const { id } = useParams()
+    const {id} = useParams();
 
     useEffect(() => {
         async function getObjectInfo() {
             try {
                 const result = await axios.get(`https://www.rijksmuseum.nl/api/nl/collection/${id}?key=${apiKey}`);
                 console.log(result);
+                setArtInfo(result);
+                setArt(result.data.artObject.objectTypes);
+                console.log(result.data.artObject.objectTypes);
             } catch (e) {
                 console.error(e);
             }
         }
+
         getObjectInfo()
     }, [])
 
 
+    function artObjectInfo(text) {
+        const newText = text.map((art, index) => <h4 key={index}>{art}</h4>);
+        return newText;
+    }
+
     return (
         <div>
-
+            <h2>{artInfo && artInfo.data.artObject.label.title}</h2>
+            <img src={artInfo && artInfo.data.artObject.webImage.url} alt="art-image" width="500px"/>
+            <h4>discription: {artInfo && artInfo.data.artObject.label.description}</h4>
+            <h4>objectTypes: </h4>
+            {artObjectInfo(art)}
         </div>
     );
 };
