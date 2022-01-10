5d7f48281936c0df18e81db8f04b5a08796bf668
MarcelB21 <marcelbrzezinski@msn.com>
2022-01-10 12:36:47 +0100

    page-size-and-page-number-to-home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import './Home.css';
 import React, {Fragment, useEffect, useState} from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 
 
 const Home = () => {
@@ -10,10 +10,29 @@ const Home = () => {
     
     const apiKey = 'IofFTSpv';
 
+    let [pageSize, setPageSize] = useState(10);
+    let handleIncrease = (event) => {
+        setPageSize(pageSize+10);
+        event.preventDefault();
+    }
+    let handleDecrease = (event) => {
+        setPageSize(pageSize-10);
+        event.preventDefault();
+    }
+
+
+    const [pageNumber, setPageNumber] = useState(1);
+    const prevPage = () => {
+        setPageNumber(pageNumber -1);
+    }
+    const nextPage = () => {
+        setPageNumber(pageNumber +1)
+    }
+
     useEffect(() => {
         async function fetchData() {
             try {
-                const result = await axios.get(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&involvedMaker=Rembrandt+van+Rijn`);
+                const result = await axios.get(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&involvedMaker=Rembrandt+van+Rijn&p=${pageNumber}&ps=${pageSize}`);
                 console.log(result.data.artObjects);
                 setArt(result.data.artObjects);
             } catch (e) {
@@ -22,12 +41,23 @@ const Home = () => {
         }
 
     fetchData()
-    }, [])
+    }, [pageSize, pageNumber])
 
 
 
     return (
         <div>
+            <div>
+                <p>{pageSize} kunstobjecten op de pagina</p>
+                <p>
+                    <button type="button" onClick={handleDecrease} disabled={pageSize<11}>
+                        Klik om te verminderen
+                    </button>
+                    <button type="button" onClick={handleIncrease} disabled={pageSize>99}>
+                        klik om te vermeerderen
+                    </button>
+                </p>
+            </div>
             {art && art.map((art, index)=>{
                 return (
                     <Fragment key={index}>
@@ -36,7 +66,7 @@ const Home = () => {
                                 <h2>Title:</h2>
                                 <h2><Link to={`/objectInfo/${art.objectNumber}`}>{art.title}</Link></h2>
                                 <h3>image:</h3>
-                                <img src={art.webImage.url} alt="art-image" width="250px"/>
+                                {art.webImage !== null? <img src={art.webImage.url} alt="art-image" width="250px"/> : ''}
                                 <h4>ID: </h4>
                                 <p>{art.id}</p>
                             </ul>
@@ -44,6 +74,16 @@ const Home = () => {
                     </Fragment>
                 )
             })}
+            <div>
+                <p>
+                    <button disabled={pageNumber<2} onClick={prevPage}>
+                        Vorige pagina
+                    </button>
+                    <button onClick={nextPage}>
+                        Volgende pagina
+                    </button>
+                </p>
+            </div>
         </div>
     );
 };
